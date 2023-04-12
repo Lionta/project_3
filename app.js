5c44e07605b313184a54b4439c210a54fa28986d
lionta <trowagundam@gmail.com>
2023-04-12 16:29:55 -0700

    properly cleaned the data, made new CSVs. created working line chart with dropdown

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Create the URL 
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+const url = "Stocks_clean.csv";
 
 // Get the data, populate the initial chart with the first dataset, and create the dropdown menu. 
-d3.json(url).then(function(data){
-    createChart(data.samples[0].id);
+d3.csv(url).then(function(data){
+    //console.log(data);
+    createChart("Microsoft");
     var dropdown = d3.select("#selDataset");
-    var names = data.names;
+    var names = ["Microsoft", "Apple", "Amazon", "IBM", "Nike"];
     names.forEach(function(name) {
         dropdown.append("option").text(name).property("value", name);
     });
@@ -13,22 +14,20 @@ d3.json(url).then(function(data){
 
 // Create a function to contain all the code that will need to happen each time the dropdown changes. 
 function createChart(id){
-    d3.json(url).then(function(data){
+    d3.csv(url).then(function(data){
         // Filter the data for only the selected ID
-        var sampleData = data.samples.filter(sample => sample.id === id)[0];
-
-        // Get the top 10 sample_values, otu_ids, and otu_labels for usage later
-        var top10Values = sampleData.sample_values.slice(0, 10).reverse();
-        var top10Ids = sampleData.otu_ids.slice(0, 10).reverse();
-        var top10Labels = sampleData.otu_labels.slice(0, 10).reverse();
+        const filteredData = data.map(function(d) {
+            return {
+              "Date": d['date'],
+              "Value": +d[id]
+            };
+        });
 
         // Create the trace for the bar chart
         var trace = {
-            x: top10Values,
-            y: top10Ids.map(id => "OTU " + id),
-            text: top10Labels,
-            type: "bar",
-            orientation: "h"
+            x: filteredData.map(function(d) { return d.Date; }),
+            y: filteredData.map(function(d) { return d.Value; }),
+            type: 'scatter'
         };
 
         // Create the data array for the plot
@@ -36,43 +35,13 @@ function createChart(id){
 
         // Configuring the layout of the bar chart
         var layout = {
-            title: "Top 10 OTUs",
-            xaxis: { title: "Sample Values" },
-            yaxis: { title: "OTU IDs" }
+            title: id+ "Stock Price",
+            xaxis: { title: "Date" },
+            yaxis: { title: "Price" }
         };
 
         // Plotting the bar chart
-        Plotly.newPlot("bar", data1, layout);
-
-
-
-
-        // Create the trace for the bubble chart 
-        var trace = {
-            x: top10Ids,
-            y: top10Values,
-            text: top10Labels,
-            mode: 'markers',
-            marker: {
-            size: top10Values,
-            color: top10Ids,
-            colorscale: 'Earth'
-            }
-        };
-
-        // Set the layout for the bubble chart
-        var layout = {
-            title: 'Bubble Chart',
-            xaxis: { title: 'OTU ID' },
-            yaxis: { title: 'Sample Values' }
-        };
-
-        // Create the data array for the plot
-        var data2 = [trace];
-
-        // Plot the chart using Plotly
-        Plotly.newPlot('bubble', data2, layout);
-
+        Plotly.newPlot("line", data1, layout);
 
 
         // Select the panel-body div using D3
@@ -81,33 +50,11 @@ function createChart(id){
         // Delete anything in the description 
         panelBody.selectAll("*").remove();
 
-        // Get the metadata for the sample
-        var metadata = data.metadata.filter(meta => meta.id == id)[0];
-
         // Create a list element and append it to the panel-body div
         var list = panelBody.append("ul");
 
         // Create a row for id and append it to the list
-        list.append("li").text("ID: " + metadata.id);
-
-        // Create a row for ethnicity and append it to the list
-        list.append("li").text("Ethnicity: " + metadata.ethnicity);
-
-        // Create a row for gender and append it to the list
-        list.append("li").text("Gender: " + metadata.gender);
-
-        // Create a row for age and append it to the list
-        list.append("li").text("Age: " + metadata.age);
-
-        // Create a row for location and append it to the list
-        list.append("li").text("Location: " + metadata.location);
-
-        // Create a row for bbtype and append it to the list
-        list.append("li").text("BB Type: " + metadata.bbtype);
-
-        // Create a row for wfreq and append it to the list
-        list.append("li").text("W Freq: " + metadata.wfreq);
-
+        //list.append("li").text("ID: " + metadata.id);
 
     });
 };
