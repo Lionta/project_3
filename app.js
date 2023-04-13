5366485fb9ed136a19c91cc89a69fba806e64de8
lionta <trowagundam@gmail.com>
2023-04-12 18:37:36 -0700

    adding new files. making the lines different colors

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 // Create the URL 
 const stocksurl = "Stocks_clean.csv";
 const gdpurl = "GDP_clean.csv";
-// Get the data, populate the initial chart with the first dataset, and create the dropdown menu. 
-d3.csv(stocksurl).then(function(data){
-    //console.log(data);
-    //createChart("Microsoft",1);
-    var dropdown = d3.select("#selDataset");
-    var names = ["Microsoft", "Apple", "Amazon", "IBM", "Nike"];
-    //names.forEach(function(name) {
-     //   dropdown.append("option").text(name).property("value", name);
-   // });
-});
 
 // Create a function to contain all the code that will need to happen each time the dropdown changes. 
 function createChart(id, type){
-    console.log(type);
     if(type == 1){
         d3.csv(stocksurl).then(function(data){
             // Filter the data for only the selected ID
@@ -29,7 +18,10 @@ function createChart(id, type){
             var trace = {
                 x: filteredData.map(function(d) { return d.Date; }),
                 y: filteredData.map(function(d) { return d.Value; }),
-                type: 'line'
+                type: 'line',
+                line: {
+                    color: getRandomColor()
+                }
             };
 
             // Create the data array for the plot
@@ -46,19 +38,6 @@ function createChart(id, type){
 
             // Plotting the line chart
             Plotly.newPlot("line", data1, layout);
-
-
-            // Select the panel-body div using D3
-            //var panelBody = d3.select(".panel-body");
-
-            // Delete anything in the description 
-            //panelBody.selectAll("*").remove();
-
-            // Create a list element and append it to the panel-body div
-            //var list = panelBody.append("ul");
-
-            // Create a row for id and append it to the list
-            //list.append("li").text("ID: " + metadata.id);
 
     });} else if(type == 3) {
         d3.csv(gdpurl).then(function(data){
@@ -145,3 +124,10 @@ function optionChanged(id, value){
     // Call the createChart function to create the new charts with the new ID
     createChart(id, value);
 }
+
+function getRandomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+};
